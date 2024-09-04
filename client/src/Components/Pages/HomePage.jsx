231fef69aa28b5520ecbc28dacaa4338006040e9
JAPH254 <omanyujapheth@gmail.com>
2024-09-04 08:40:21 +0300

    fix: add the navigation and time format

--- client/src/Components/Pages/HomePage.jsx
+++ client/src/Components/Pages/HomePage.jsx
@@ -4,38 +4,108 @@ import "./Pages.css";
 import { fetchProducts } from "../../features/products/productActions";
 import { fetchProfile } from "../../features/auth/profileSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import AddProducts from "../products/AddProduct";
 import AddCategory from "../products/AddCategory";
 import { fetchProductsCategory } from "../../features/products/productActions";
 import ManageCategories from "../products/ManageCategories";
+import { logout } from "../../features/auth/authSlice";
 const HomePage = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { token } = useSelector((state) => state.auth);
   const { products } = useSelector((state) => state.products);
   const status = useSelector((state) => state.products.status);
   const { user, isLoading, error } = useSelector((state) => state.profile);
   const [profile_picture, setProfilePicture] = useState("./avatar.png");
 
+  console.log(user);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
   useEffect(() => {
-    if (status === "idle") {
-      dispatch(fetchProducts());
-      dispatch(fetchProfile());
-      setProfilePicture(user?.profile_picture);
+    const fetchData = async () => {
+      if (status === "idle") {
+        await dispatch(fetchProducts());
+        await dispatch(fetchProfile());
+      }
+    };
+    fetchData();
+  }, [status, dispatch]);
+
+  useEffect(() => {
+    const updateProfilePicture = async () => {
+      if (user && user.profile_picture) {
+        setProfilePicture(user.profile_picture);
+      }
+    };
+    updateProfilePicture();
+  }, [user]);
+
+  useEffect(() => {
+    const handleLogout = async () => {
+      if (!token) {
+        await dispatch(logout());
+        navigate("/");
+      }
+    };
+    handleLogout();
+  }, [token, dispatch, navigate]);
+
+  if (isLoading) {
+    return (
+      <>
+        <p>loading</p>
+      </>
+    );
+  }
+
+  function timeAgo(date) {
+    const now = new Date();
+    const seconds = Math.floor((now - new Date(date)) / 1000);
+
+    const intervals = {
+      year: 31536000,
+      month: 2592000,
+      week: 604800,
+      day: 86400,
+      hour: 3600,
+      minute: 60,
+    };
+
+    for (const [key, value] of Object.entries(intervals)) {
+      const interval = Math.floor(seconds / value);
+      if (interval >= 1) {
+        return interval === 1
+          ? `${interval} ${key} ago`
+          : `${interval} ${key}s ago`;
+      }
     }
-  }, [status, dispatch, profile_picture]);
+
+    return "just now";
+  }
   return (
     <div className="">
       <div className="bg-gradient-to-r from-indigo-500 to-purple-600">
         {" "}
         <header className="flex justify-between items-center mb-1">
           <div className="logo">
-            <Link to="/" className="text-white text-2xl font-semibold flex flex-column items-center">
-              <img src="./logo.png" alt="Heaviety logo" className="h-16 w-16 rounded-full object-cover" />
+            <Link
+              to="/"
+              className="text-white text-2xl font-semibold flex flex-column items-center"
+            >
+              <img
+                src="./logo.png"
+                alt="Heaviety logo"
+                className="h-16 w-16 rounded-full object-cover"
+              />
               <small className="ml-2 bg-gradient-to-r from-teal-500 to-purple-600 bg-clip-text text-transparent">
                 Heaviety
               </small>
             </Link>
-
-
           </div>
           <div className="flex items-center space-x-4">
             <input
@@ -45,9 +115,106 @@ const HomePage = () => {
             />
             <AddProducts />
             <div className="user-menu relative">
-              <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
-                {user?.first_name}
-              </button>
+              <div className="relative inline-block text-left">
+                <div>
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                    id="menu-button"
+                    aria-expanded={isOpen}
+                    aria-haspopup="true"
+                    onClick={toggleDropdown} // Toggle dropdown on click
+                  >
+                    {user?.first_name}
+                    <svg
+                      className="-mr-1 h-5 w-5 text-gray-400"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </div>
+
+                {isOpen && ( // Conditionally render the dropdown menu
+                  <div
+                    className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="menu-button"
+                  >
+                    <div className="py-1" role="none">
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700"
+                        role="menuitem"
+                        id="menu-item-0"
+                      >
+                        Profile
+                      </a>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700"
+                        role="menuitem"
+                        id="menu-item-1"
+                      >
+                        Edit
+                      </a>
+                    </div>
+                    <div className="py-1" role="none">
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700"
+                        role="menuitem"
+                        id="menu-item-2"
+                      >
+                        Archive
+                      </a>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700"
+                        role="menuitem"
+                        id="menu-item-3"
+                      >
+                        Move
+                      </a>
+                    </div>
+                    <div className="py-1" role="none">
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700"
+                        role="menuitem"
+                        id="menu-item-4"
+                      >
+                        Share
+                      </a>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700"
+                        role="menuitem"
+                        id="menu-item-5"
+                      >
+                        Settings
+                      </a>
+                    </div>
+                    <div className="py-1" role="none">
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700"
+                        role="menuitem"
+                        id="menu-item-6"
+                      >
+                        Logout
+                      </a>
+                    </div>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </header>{" "}
@@ -114,24 +281,30 @@ const HomePage = () => {
                       <img
                         src={user?.profile_picture}
                         alt=""
-                        className="w-24 h-2 rounded-full mx-auto border-2 border-blue-500"
+                        className="w-12 h-12 rounded-full mx-auto border-2 border-blue-500"
                       />
                     </div>
                     <div className="col-6">
-                      <p className="text-sm text-gray-500">
+                      <p className="">
                         Posted by {product.user_full_name}
                       </p>
+                      <small className="text-gray-500">
+                        {timeAgo(product.created_at)}
+                      </small>
                     </div>
                   </div>
-                  <p className="text-gray-600">{product.description}</p>
+                  <h2 className="text-xl font-semibold mt-2 ">
+                    {product.title}
+                  </h2>
+                  <small className="text-primary">
+                    {product.stock} available on stock
+                  </small>
+                  <p className="">{product.description}</p>
                   <img
                     src={product.image}
                     alt={product.title}
                     className="w-full h-48 object-cover rounded"
                   />
-                  <h2 className="text-xl font-semibold mt-2">
-                    {product.title}
-                  </h2>
 
                   <p className="text-gray-800 font-bold">${product.price}</p>
                 </div>
@@ -166,16 +339,10 @@ const HomePage = () => {
                 Logout
               </button>
             </div>
-            <Link
-              to="/notifications"
-              className="text-success p-1"
-            >
+            <Link to="/notifications" className="text-success p-1">
               View Notifications
             </Link>
-            <Link
-              to="/messages"
-              className="text-success p-1"
-            >
+            <Link to="/messages" className="text-success p-1">
               Check Messages
             </Link>
           </div>
